e9485e1695f3601f237b379098e355ec8c385322
Teewalee A. <48087481+Teewalee@users.noreply.github.com>
2025-01-13 20:47:06 -0500

    Data Type

--- demo.rb
+++ demo.rb
@@ -1,9 +1,7 @@
-character_name = "John"
-character_age = "35"
-
-puts ("There once was a man named " + character_name + ".")
-puts ("He was " + character_age + " years old.")
-
-character_name = "Tom"
-puts ("He really liked the name " + character_name)
-puts ("but didn't like being " + character_age + ".")
+# Data Type
+name = "Mike"   # String
+age = 75        # Int
+gpa = 3.2       # Float
+ismale = True   # Boolean
+istall = false
+flaws = nil     # Null (No value)
