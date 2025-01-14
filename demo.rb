550608523376dc48509e5c16155e24784a5114ef
Teewalee A. <48087481+Teewalee@users.noreply.github.com>
2025-01-13 20:44:42 -0500

    change value in variable

--- demo.rb
+++ demo.rb
@@ -3,5 +3,7 @@ character_age = "35"
 
 puts ("There once was a man named " + character_name + ".")
 puts ("He was " + character_age + " years old.")
+
+character_name = "Tom"
 puts ("He really liked the name " + character_name)
 puts ("but didn't like being " + character_age + ".")
